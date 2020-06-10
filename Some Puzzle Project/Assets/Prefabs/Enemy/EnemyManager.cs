bc4f7e4e647fd280008f036bc28e597c6fb83f82
William Henarto T <whenarto@gmail.com>
2020-06-11 04:44:42 +0900

    first commit

---
+++ Some Puzzle Project/Assets/Prefabs/Enemy/EnemyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public float lookupRadius = 10f;
+    Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameManager.Instance.getCurrentPlayer.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookupRadius)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position-transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+    
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookupRadius);
+    }
+}
---
+++ Some Puzzle Project/Assets/Utilities Script/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField]private GameObject currentPlayer;
+
+    public GameObject getCurrentPlayer
+    {
+        get{return currentPlayer;}
+    }
+	
+    private void Awake()
+    {
+        Assert.IsNotNull(currentPlayer);
+    }
+}
